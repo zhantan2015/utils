201503625bf52961f75b195ec6275ff38b8e9726
zhantan2015 <zhantan2015@gmail.com>
2024-01-31 23:51:38 +0800

    init...

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import typescript from 'rollup-plugin-typescript2';
+import clear from 'rollup-plugin-clear'
+export default {
+    input: 'src/index.ts',
+    output: {
+        file: "dist/index.js"
+    },
+    plugins: [
+        clear({ targets: ["dist"] }),
+        typescript({ tsconfig: "./tsconfig.json" })
+    ]
+};
